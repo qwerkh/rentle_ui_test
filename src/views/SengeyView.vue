87da662c1f197fdb9137e7135d62814949562893
sengeys <sengeys@gmail.com>
2024-12-06 00:50:14 +0700

    Sengey : Update SengeyView.vue

---
+++ src/views/SengeyView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>This is Sengey page</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
